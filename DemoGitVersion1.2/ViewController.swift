3d8f9c5f247182da6c8246d1e8a4c802c3d7bbc2
AbhinavSrivastava <abhinav.srivastava@caglobal.com>
2024-01-16 20:20:59 +0530

    ewrgtewrt

--- DemoGitVersion1.2/ViewController.swift
+++ DemoGitVersion1.2/ViewController.swift
@@ -8,46 +8,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Anmol")
-        print("Anmol")
-        print("Anmol")
-        print("Anmol")
-        print("Anmol")
-        print("Anmol")
-        print("Anmol")
-        print("Anmol13")
-        print("Anmo14l")
-        print("Anmol15")
-        
-        print("Anmol is a good boy")
-        print("Today i am not fell good......")
-        print("Dhoodh piyo bets!!!!")
-        print("Anmol")
-        
-        
-        print("Dhoodhu Pita Hai India")
-        print("Dhoodhu Pita Hai India")
-        print("Dhoodhu Pita Hai India")
-        print("Dhoodhu Pita Hai India")
-        print("Dhoodhu Pita Hai India")
-        print("Dhoodhu Pita Hai India")
-        print("Dhoodhu Pita Hai India")
-        print("Dhoodhu Pita Hai India")
-        print("Dhoodhu Pita Hai India")
-        print("Dhoodhu Pita Hai India")
-        
-        print("Sab Bakwass hai Juth bolte hai nakli dhuudh hai")
-        print("Upar likha sab jhooth hai krpya na maane")
-
-        
-        print("na mano bhai tume khud pta chal jayega.....! &_&")
-        print("Sab paise ka khel hai Moh Maya Hai")
-        
-        print("Voh toh hai hi inko bhi pta hai fir bhi logo ko kharid re hai quality bhi nai hai sab bakwaas hai nakli dhuudh.....@!")
-        
-        print("Advertisement hai bas Dhoodh Amul Pita Hai India ^_^")
-        
-        print("Roz")
+       print("++++++++++++++++++++++++++++++================+++++++++++++++++++++++++++++++++++++++")
     }
 
 
