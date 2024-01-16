4adc82fbc12c54c78e7020ebb4affaa9a2b1d207
Anmol <shobhittestmail@gmail.com>
2024-01-16 19:40:27 +0530

    sfgh,k

--- DemoGitVersion1.2/ViewController.swift
+++ DemoGitVersion1.2/ViewController.swift
@@ -1,9 +1,6 @@
-//
 //  ViewController.swift
 //  DemoGitVersion1.2
-//
 //  Created by caglobal on 16/01/24.
-//
 
 import UIKit
 
@@ -12,6 +9,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Amul")
+        print("Amul Dhuudh Pita tha india")
     }
 
 
